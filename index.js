a00f6b665a3c386a7a1a9b897a6efe1596bbe7aa
Ujjwal <ujjwal.jain116@gmail.com>
2024-10-01 12:23:23 +0530

    Server code

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`User Connected: ${socket.id}`);
+
+  socket.on("join_room", (data) => {
+    socket.join(data.roomNumber);
+  });
+
+  socket.on("send_message", (data) => {
+    console.log(data, "daada");
+    
+    socket.to(data.room).emit("receive_message", data);
+    // socket.broadcast.emit("receive_message", data.message);
+    // console.log(data,"msg");
+    
+  });
+});
+
+server.listen(3001, () => {
+  console.log("SERVER IS RUNNING...");
+});
